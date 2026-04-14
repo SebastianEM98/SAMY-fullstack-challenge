e9cf43a9841745cfa296291458cd6b7bcc76b462
Sebastian Echeverri <sebas.eche98@gmail.com>
2026-04-13 22:06:33 -0500

    Auth service created

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -2,5 +2,6 @@ export const env = {
     port: process.env.PORT || 4000,
     databaseUrl: process.env.DATABASE_URL || '',
     frontendUrl: process.env.FRONTEND_URL || '',
+    reqresApiKey: process.env.REQRES_API_KEY || '',
     nodeEnv: process.env.NODE_ENV || 'development',
 };
